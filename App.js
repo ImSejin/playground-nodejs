ba72c5bd9426f13a26d430ecb4ebc774de6bcd6d
ImSejin <hackeres123@gmail.com>
2021-01-06 15:11:55 +0900

    🎨 Format: code

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-const {screenshot} = require('./lib/Screenshot');
 const {parseArguments} = require('./ArgsParser');
 
 const arguments = parseArguments(process.argv);
 if ((arguments.type || arguments.t) === 'screenshot') {
+    const {screenshot} = require('./lib/Screenshot');
     (async () => await screenshot(arguments.name || arguments.n))();
 }
--- ArgsParser.js
+++ ArgsParser.js
@@ -6,14 +6,13 @@
  */
 const regex = /^(?<prefix>-{1,2})\w+=?/;
 
-module.exports.parseArguments = args => {
-    const arguments = args.slice(2)
+exports.parseArguments = args => {
+    return args.slice(2)
         .filter(it => regex.test(it))
         .map(it => {
             const matched = it.match(regex);
             return matched.input.replace(matched.groups.prefix, '').split('=')
         })
-        .map(arr => ({[arr[0]]: arr[1] || true}));
-
-    return arguments.reduce((acc, cur) => ({...acc, ...cur}), {});
-};
+        .map(arr => ({[arr[0]]: arr[1] || true}))
+        .reduce((acc, cur) => ({...acc, ...cur}), {});
+}
