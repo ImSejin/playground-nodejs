43e942ea8f6579b1173c62418c33c87fe56db6c5
ImSejin <hackeres123@gmail.com>
2021-01-07 06:28:56 +0900

    🐞 Fix: library of categorization in accepting paths

--- App.js
+++ App.js
@@ -6,5 +6,9 @@ if ((arguments.type || arguments.t) === 'screenshot') {
     const {screenshot} = require('./lib/Screenshot');
     (async () => await screenshot(arguments.name || arguments.n))();
 }
+if ((arguments.type || arguments.t) === 'categorization') {
+    const {categorize} = require('./lib/Categorization');
+    categorize(arguments.path || arguments.p);
+}
 
 (async () => await calendar.generate(550, 340))();
