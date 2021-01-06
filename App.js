d7e08b825e27986988c3ceb4c2ddff23c2a9292a
ImSejin <hackeres123@gmail.com>
2021-01-06 17:58:02 +0900

    ✨ Add: library for generator of relay calendar

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 const {parseArguments} = require('./ArgsParser');
+const calendar = require('./lib/CalendarGenerator');
 
 const arguments = parseArguments(process.argv);
 if ((arguments.type || arguments.t) === 'screenshot') {
     const {screenshot} = require('./lib/Screenshot');
     (async () => await screenshot(arguments.name || arguments.n))();
 }
+
+(async () => await calendar.generate(550, 340))();
