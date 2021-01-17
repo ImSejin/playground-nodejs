f2ab26d83b30519ea17f6b89b90bb878135413d2
ImSejin <hackeres123@gmail.com>
2021-01-17 21:20:42 +0900

    🎨 Format: code

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 const {parseArguments} = require('./ArgsParser');
 
-const arguments = parseArguments(process.argv);
-if ((arguments.type || arguments.t) === 'screenshot') {
-    const {screenshot} = require('./lib/Screenshot');
-    (async () => await screenshot(arguments.name || arguments.n))();
-}
-if ((arguments.type || arguments.t) === 'categorization') {
-    const {categorize} = require('./lib/Categorization');
-    categorize(arguments.path || arguments.p);
-}
+const {t, type, n, name, p, path} = parseArguments(process.argv);
 
-const calendar = require('./lib/CalendarGenerator');
-// (async () => await calendar.generate(550, 340))();
+switch (t || type) {
+  case 'screenshot':
+    const {screenshot} = require('./lib/Screenshot');
+    (async () => await screenshot(name || n))();
+    break;
+
+  case 'categorization':
+    const {categorize} = require('./lib/Categorization');
+    categorize(path || p);
+    break;
+
+  case 'calendar':
+    const calendar = require('./lib/CalendarGenerator');
+    (async () => await calendar.generate(550, 340))();
+    break;
+
+  case 'webtoon':
+    const {renameNaver} = require('./lib/renameWebtoons');
+    renameNaver(path || p);
+    break;
+
+  default:
+    break;
+}
